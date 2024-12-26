ea2b2e6da23e1aeb91143b82194c234f1480a6ab
moodle <moodle@moodle24.bk-rheinbach.net>
2024-12-26 11:19:54 +0100

    fetch dazu

---
+++ fetch-Moodle-Updates.sh
@@ -0,0 +1,5 @@
+   #!/bin/sh
+   git fetch upstream
+   for BRANCH in  MOODLE_311_STABLE MOODLE_401_STABLE MOODLE_405_STABLE master; do
+       git push origin refs/remotes/upstream/$BRANCH:$BRANCH
+   done
